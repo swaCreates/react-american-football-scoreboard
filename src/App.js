4ccf3e30f6bbc11e1606c8f8ed4bdc5ca8ab3861
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2019-12-18 21:27:49 -0500

    added timer and buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
+  // setting scores
   const [homeScore, setHomeScore]= useState(0);
   const [awayScore, setAwayScore]= useState(0);
+
+  // setting timer
+  const [seconds, setSeconds]= useState(0);
+  const [isActive, setIsActive]= useState(false);
+
+  function toggle(){
+    setIsActive(!isActive);
+  }
+
+  function reset(){
+    setSeconds(0);
+    setIsActive(false);
+  }
+
+  useEffect(function() {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(function() {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return function() {clearInterval(interval)};
+  }, [isActive, seconds]);
+  
 
   return (
     <div className="container">
@@ -20,7 +47,9 @@ function App() {
 
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{`00:`+ `0` + seconds}</div>
+          <button onClick={toggle} className='buttons' className='newButtons'>{isActive ? 'Pause' : 'Start'}</button>
+          <button onClick={reset} className='buttons' className='newButtons'>Reset</button>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
